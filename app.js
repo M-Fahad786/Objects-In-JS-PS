c44ea88fcb370d58dec21843c84535bd2220036d
M-Fahad786 <fahad.zahid2004@gmail.com>
2022-09-01 16:45:30 +0500

    Objects PS

---
+++ app.js
@@ -0,0 +1,92 @@
+/*
+let obj = {
+  Name: "Tesla X",
+  Color: "Black",
+  Model: 2021,
+  Manufacturer: "Tesla",
+  Specifications: "Auto Drive Feature",
+  Wheels: "Alloy",
+};
+console.log(obj);
+*/
+
+/*
+let obj2 = {
+  names: ["Fahad", "Zahid", "Mustafa"],
+  age: [18, 45, 10],
+};
+console.log(obj2);
+*/
+
+/*
+let obj3 = {};
+obj3.name = "Fahad";
+obj3.age = 18;
+obj3.studies = "Bachelor";
+obj3.city = "Karachi";
+console.log(obj3);
+*/
+
+/*
+let obj4 = {
+  Name: "Tesla X",
+  Color: "Black",
+  Model: 2021,
+  Manufacturer: "Tesla",
+  Specifications: "Auto Drive Feature",
+  Wheels: "Alloy",
+};
+for (let key in obj4) {
+  console.log(
+    "The following value of" + " " + key + " " + "is" + " " + obj4[key]
+  );
+}
+*/
+
+/*
+let obj5= {
+  Name: "Tesla X",
+  Color: "Black",
+  Model: 2021,
+  Manufacturer: "Tesla",
+  Specifications: "Auto Drive Feature",
+  Wheels: "Alloy",
+};
+console.table(obj5)
+*/
+
+/*
+function Plan(name, price, timings, pages) {
+  this.name = name;
+  this.price = price;
+  this.timings = timings;
+  this.pages = pages;
+}
+let obj = new Plan("Basic" , "20$" , "6hours" , "20");
+let xyz = new Plan("Pro" , "20$" , "6hours" , "20");
+
+console.log("price" in obj)
+*/
+
+/*
+let myQuestions = [
+  {
+    question: "What is Your Name?",
+    answers: {
+      a: "Fahad",
+      b: "Zahid",
+      c: "Mustafa",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "What is Your Age?",
+    answers: {
+      a: "15",
+      b: "18",
+      c: "17",
+    },
+    correctAnswer: "b",
+  },
+];
+*/
